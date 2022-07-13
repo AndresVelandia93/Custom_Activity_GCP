22f2ac6a6a0835c6efaf2f69558d6e48d5516c36
AndresVelandia93 <andresvelandia93@hotmail.com>
2022-07-13 01:58:05 -0500

    Primer Commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const JWT = require(path.join(__dirname, 'lib', 'jwt.js'));
 const Pkg = require(path.join(__dirname, '../', 'package.json'));
 
 const app = express();
+let activity = null;
+const connection = new Postmonger.Session();
 
 app.set('port', process.env.PORT || 3000);
 
@@ -87,3 +89,61 @@ function extractFieldName(field) {
 
 
 app.listen(app.get('port'), () => console.log('App listening on port ' + app.get('port')))
+
+function requestedInteraction(payload) {
+  console.log('-------- requestedInteraction --------');
+  console.log('payload\n', JSON.stringify(payload, null, 4));
+  console.log('requestInteraction', payload);
+  console.log('--------------------------------------');
+
+  let selectedValue;
+
+  // determine the selected item (if there is one)
+  if(activity.arguments.execute.inArguments) {
+      const existingSelection = activity.arguments.execute.inArguments[0].discount ?? activity.arguments.execute.inArguments[0].discountCode;
+
+      if(existingSelection.split('.').length == 3) {
+          selectedValue = existingSelection.split('.')[1];
+      }
+  }
+
+  // populate the select dropdown.
+  const selectElement = document.getElementById('discount-code');
+
+  payload.activities.forEach(a => {
+      if(a.schema && a.schema.arguments && a.schema.arguments.execute &&
+          a.schema.arguments.execute.outArguments && a.schema.arguments.execute.outArguments.length > 0) {
+          a.schema.arguments.execute.outArguments.forEach(inArg => {
+              if(inArg.discountCode) {
+                  let option = document.createElement("option");
+                  option.text = `${a.name} - (${a.key})`;
+                  option.value = a.key;
+                  selectElement.add(option);
+              }
+          });
+      }
+  });
+
+  // Display the warning if there is an issue, otherwise, display the
+  if(selectElement.childElementCount == 0) {
+      document.getElementById('main-form').style.display = 'hidden';
+      document.getElementById('warning').style.display = 'block';
+  } else {
+      document.getElementById('main-form').style.display = 'block';
+      document.getElementById('warning').style.display = 'hidden';
+
+      // if we have a previously selected value, repopulate that value.
+      if(selectedValue) {
+          const selectOption = selectElement.querySelector(`[value='${selectedValue}']`);
+
+          if (selectOption) {
+              selectOption.selected = true;
+          } else {
+              console.log('Could not select value from list', `[value='${selectedValue}]'`);
+          }
+      }
+
+      // let journey builder know the activity has changes
+      connection.trigger('setActivityDirtyState', true);
+  }
+}
